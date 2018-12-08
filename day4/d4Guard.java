edce1357add6c12d8dca7101a2e4c78f5d79b845
Ricardo Vogel <info@ricardovogel.nl>
2018-12-08 13:38:52 +0100

    Day 4
    Does not give an actual output, you have to use the debugger and some smart breakpoints. I will make a better version later.

---
+++ day4/d4Guard.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class d4Guard {
+	private int number;
+	private int minAsleep;
+	public List<String> strings;
+	public List<String> times;
+	public int bar = -1;
+
+	public d4Guard(int number, int minAsleep) {
+		this.number = number;
+		this.minAsleep = minAsleep;
+		strings = new ArrayList<String>();
+		times = new ArrayList<String>();
+	}
+
+	public void add(String stringToAdd) {
+		strings.add(stringToAdd);
+	}
+
+	public void addtime(String start, String stop) {
+		times.add(start + "-" + stop);
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public int getMinAsleep() {
+		return minAsleep;
+	}
+
+	public void addAsleep(int timeToAdd) {
+		minAsleep += timeToAdd;
+	}
+
+	public String toString() {
+		return "ID: " + number + " min asleep: " + minAsleep + "\n" + strings + "\n" + times + "\n" + foo();
+	}
+
+	public int foo() {
+		int[] foo = new int[60];
+
+		for (String str : times) {
+			int start = Integer.parseInt(str.split("-")[0].split(":")[1]);
+			int stop = Integer.parseInt(str.split("-")[1].split(":")[1]);
+
+			for (int i = start; i != stop; i++) {
+				foo[i]++;
+			}
+		}
+		int max = 0;
+		for (int i = 0; i != 60; i++) {
+			if (foo[i] > max) {
+				max = foo[i];
+			}
+		}
+		if (max == 18) {
+			System.out.println("");
+		}
+
+		return max;
+	}
+
+}
---
+++ day4/d4c2.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class d4c2 {
+
+	static List<d4Guard> guardList;
+	private static Scanner sc;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		guardList = new ArrayList<d4Guard>();
+
+		sc = new Scanner(new File("d4input.txt"));
+		List<String> inputList = new ArrayList<String>();
+
+		while (sc.hasNextLine()) {
+			inputList.add(sc.nextLine());
+		}
+
+		Collections.sort(inputList);
+		d4Guard prev = null;
+		int sleepH = 0;
+		int sleepM = 0;
+		for (String str : inputList) {
+			if (str.contains("#")) {
+				int id = Integer.parseInt(str.split(" ")[3].replace("#", ""));
+				boolean alreadyExists = false;
+				for (d4Guard gu : guardList) {
+					if (gu.getNumber() == id) {
+						prev = gu;
+						alreadyExists = true;
+						break;
+					}
+				}
+				if (!alreadyExists) {
+					d4Guard g = new d4Guard(id, 0);
+					guardList.add(g);
+					prev = g;
+				}
+				prev.add(str);
+			} else if (str.contains("falls asleep")) {
+				sleepH = Integer.parseInt(str.split(" ")[1].split(":")[0]);
+				sleepM = Integer.parseInt(str.split(" ")[1].split(":")[1].replace("]", ""));
+				prev.add(str);
+
+			} else if (str.contains("wakes up")) {
+				int wakeH = Integer.parseInt(str.split(" ")[1].split(":")[0]);
+				int wakeM = Integer.parseInt(str.split(" ")[1].split(":")[1].replace("]", ""));
+
+				prev.addAsleep((wakeH - sleepH) * 60);
+				prev.addAsleep(wakeM - sleepM);
+				prev.addtime(sleepH + ":" + sleepM, wakeH + ":" + wakeM);
+				sleepH = 0;
+				sleepM = 0;
+				prev.add(str);
+			}
+		}
+	
+		Collections.sort(guardList, new Comparator<d4Guard>() {
+			@Override
+			public int compare(d4Guard a, d4Guard b) {
+				if (a.foo() > b.foo()) {
+					return -1;
+				} else if (a.foo() < b.foo()) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+		});
+
+		System.out.println(guardList.get(0));
+	}
+}
