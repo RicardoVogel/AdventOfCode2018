491f2066064ef93ff30cd6e2a0e847e67fffed20
Ricardo Vogel <info@ricardovogel.nl>
2018-12-08 16:23:04 +0100

    Day 8 Challenge 1

---
+++ day8/d8c1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class d8c1 {
+	static Scanner sc;
+	static int tot;
+
+	public static void main(String[] args) throws FileNotFoundException {
+		sc = new Scanner(new File("d8input.txt"));
+		foo();
+		System.out.println(tot);
+		sc.close();
+	}
+
+	public static void foo() {
+		int children = sc.nextInt();
+		int entries = sc.nextInt();
+		for (int i = 0; i < children; i++) {
+			foo();
+		}
+
+		for (int i = 0; i < entries; i++) {
+			tot += sc.nextInt();
+		}
+	}
+
+}
